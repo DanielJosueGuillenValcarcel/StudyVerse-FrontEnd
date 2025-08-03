493bc5e1676b224648b055b57d0416e04f09b4ef
DanielJosueGuillenValcarcel <dguillenva@unsa.edu.pe>
2025-08-03 14:08:29 -0500

    feat: create model regiter user

---
+++ studyverse/src/app/features/auth/models/register-user.model.ts
@@ -0,0 +1,9 @@
+export interface RegisterUser {
+    name: string;
+    lastName: string;
+    username: string;
+    email: string;
+    password: string;
+    password2: string;
+    birthday: Date;
+}
