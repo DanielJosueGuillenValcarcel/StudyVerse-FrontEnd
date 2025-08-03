246e55ed606486b0f32bcf0bf20e93c73ba38783
DanielJosueGuillenValcarcel <dguillenva@unsa.edu.pe>
2025-08-03 16:39:01 -0500

    feat: add provider firestore

--- studyverse/src/app/app.config.ts
+++ studyverse/src/app/app.config.ts
@@ -5,6 +5,7 @@ import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
@@ -17,6 +18,7 @@ export const appConfig: ApplicationConfig = {
     provideFirebaseApp(() => initializeApp(
       environment.firebaseConfig
     )),
-    provideAuth(() => getAuth())
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ]
 };
