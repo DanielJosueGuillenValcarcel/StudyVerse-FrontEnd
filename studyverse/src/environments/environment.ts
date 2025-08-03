fa56bd3a66936ef5dc9b2b840686bf73fc95739a
DanielJosueGuillenValcarcel <dguillenva@unsa.edu.pe>
2025-08-03 13:43:22 -0500

    chore: add environments for firebase

---
+++ studyverse/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyDXUaA5GoI-l8zPwNgnOTjp0L4VrjHmyFQ",
+    authDomain: "studyverse-f3cc6.firebaseapp.com",
+    projectId: "studyverse-f3cc6",
+    storageBucket: "studyverse-f3cc6.firebasestorage.app",
+    messagingSenderId: "619172401410",
+    appId: "1:619172401410:web:727f703ee50f045ad4721b",
+    measurementId: "G-CT4VRBM4ZF",
+  },
+};
