10425d1394445b157d271ee2050a74400a2b9706
DanielJosueGuillenValcarcel <dguillenva@unsa.edu.pe>
2025-08-04 12:10:05 -0500

    feat: create service for auth user

---
+++ studyverse/src/app/features/auth/services/auth-service.ts
@@ -0,0 +1,37 @@
+import { Injectable, inject } from '@angular/core';
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, UserCredential } from '@angular/fire/auth';
+import { Firestore, doc, where, query, getDocs, setDoc, collection } from '@angular/fire/firestore';
+import { RegisterUserModel } from '../models/register-user.model';
+import { LoginUserModel } from '../models/login-user.model';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private auth = inject(Auth);
+  private firestore = inject(Firestore);
+  
+  registerUser(user: RegisterUserModel) : Promise<UserCredential> {
+    return createUserWithEmailAndPassword(this.auth, user.email, user.password);
+  }
+
+  loginUser(user: LoginUserModel) : Promise<UserCredential> {
+    return signInWithEmailAndPassword(this.auth, user.email, user.password);
+  }
+
+  async usernameExist(username: string) : Promise<boolean>{
+    const ref = collection(this.firestore, 'profile');
+    const q = query(ref, where('username', "==", username));
+    const result = await getDocs(q);
+    return !result.empty;
+  }
+
+  saveUserProfile(uid: string, data: any): Promise<void> {
+    return setDoc(doc(this.firestore, 'profile', uid), data);
+  }
+
+  async logout(): Promise<void> {
+    await signOut(this.auth);
+  }
+}
