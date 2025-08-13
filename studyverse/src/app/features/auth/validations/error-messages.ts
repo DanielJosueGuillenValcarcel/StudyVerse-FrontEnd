6022a80b71ec1e78d8c05c5fe70dc99ed5fe81b0
DanielJosueGuillenValcarcel <dguillenva@unsa.edu.pe>
2025-08-12 23:35:51 -0500

    add dict of messages validations

---
+++ studyverse/src/app/features/auth/validations/error-messages.ts
@@ -0,0 +1,9 @@
+export const AUTH_VALIDATION_MESSAGES: Record<string, string> = {
+    email: 'Correo Inválido',
+    minlength: 'Muy corto',
+    maxlength: 'Muy largo',
+    noSpaces: 'No debe contener espacios',
+    passwordsMismatch: 'Las contraseñas no coinciden',
+    usernameTaken: 'El nombre de usuario ya existe',
+    emailExist: 'El correo electrónico ya existe',
+};
